ff9efd895eb273700134876892e628fe0f7d1c1a
rishabh-gupta-au13 <rgp02071993@gmail.com>
2024-01-20 13:47:39 +0530

    setup the context

---
+++ 10todocontextLocal/src/context/ToDoContext.js
@@ -0,0 +1,26 @@
+import { useContext,createContext } from "react";
+
+export const ToDoContext=createContext({
+    todos:[{
+        id:1,
+        todo:"Todo msg",
+        completed:false
+    }],
+    addTodo:(todo)=>{},
+    updateTodo:(id,todo)=>{
+
+    },
+    deleteTodo:(id)=>{
+
+    },
+    toggleComplete:(id)=>{
+
+    }
+
+})
+
+export const useTodo=()=>{
+    return useContext(ToDoContext)
+}
+
+export const Todoprovider=ToDoContext.Provider
