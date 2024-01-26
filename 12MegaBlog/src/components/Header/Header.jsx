2a364420b49a895f9cda82ee927bdcfda365104b
rishabh-gupta-au13 <rgp02071993@gmail.com>
2024-01-26 19:50:46 +0530

    added forward ref

--- 12MegaBlog/src/components/Header/Header.jsx
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Container,Logo,Logout } from '../index';
 import { Link } from 'react-router-dom';
-import { UseSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 
 function Header() {
 
-  const authStatus= UseSelector((state)=>{
+  const authStatus= useSelector((state)=>{
     state.auth.status
   })
   const navigate=useNavigate()
