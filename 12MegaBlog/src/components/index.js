c0cfd3529a8969562ded476a2a4d956a98a6caa2
rishabh-gupta-au13 <rgp02071993@gmail.com>
2024-01-26 22:57:40 +0530

    added login from

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,11 +3,14 @@ import Footer from "./Footer/Footer";
 import Container from "postcss/lib/container";
 import Logo from "./Logo";
 import Logout from "./Header/Logout";
+import Input from "./Input.jsx"
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    Logout
+    Logout,
+    Input
+    
 }
